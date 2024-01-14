a4e63f30c158e7387d18d9c87643c72913451e5f
Adit Hernowo <adith@Adits-MacBook-Air.local>
2024-01-14 11:30:10 +0700

    add slidinguppanel

--- lib/utils/theme_color.dart
+++ lib/utils/theme_color.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
-Color btnColor = Color(0xff65B741);
-Color textSecondary = Color(0xffB1BBC7);
-Color colorText = Color(0xff201600);
-Color colorABAB = Color(0xffABABAB);
-Color color332 = Color(0xff332C39);
-Color color393 = Color(0xff393E46);
-Color color7b84 = Color(0xff7B8490);
+Color btnColor = const Color(0xff65B741);
+Color textSecondary = const Color(0xffB1BBC7);
+Color colorText = const Color(0xff201600);
+Color colorABAB = const Color(0xffABABAB);
+Color color332 = const Color(0xff332C39);
+Color color393 = const Color(0xff393E46);
+Color color7b84 = const Color(0xff7B8490);
 
 TextStyle text12Regular =
     TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: color393);
@@ -22,6 +22,9 @@ TextStyle text18Semi393 =
 
 TextStyle text24Medium =
     TextStyle(fontSize: 24, fontWeight: FontWeight.w500, color: color332);
+
+TextStyle text24BoldGreen =
+    TextStyle(fontSize: 24, fontWeight: FontWeight.w700, color: btnColor);
 
 TextStyle text24Medium393 =
     TextStyle(fontSize: 24, fontWeight: FontWeight.w500, color: color393);
